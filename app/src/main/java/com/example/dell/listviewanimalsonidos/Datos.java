bfe96b9094a3b7dc69d9044f9c288e368c9cdc05
manologariza <32630874+manologariza@users.noreply.github.com>
2017-11-11 08:01:27 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dell/listviewanimalsonidos/Datos.java
@@ -0,0 +1,59 @@
+package com.example.dell.listviewanimalsonidos;
+
+import java.io.Serializable;
+
+/**
+ * Created by dell on 23/10/2017.
+ */
+
+public class Datos implements Serializable {
+    private int id;
+    private String titulo;
+    private String detalle;
+    private int imagen;
+    private int sonido;
+
+    public Datos(int id, String titulo, String detalle, int imagen, int sonido) {
+        this.id = id;
+        this.titulo = titulo;
+        this.detalle = detalle;
+        this.imagen = imagen;
+        this.sonido = sonido;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
+    public int getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(int imagen) {
+        this.imagen = imagen;
+    }
+
+    public int getSonido() { return sonido;}
+
+    public  void setSonido(int sonido) { this.sonido = sonido; }
+}
---
+++ app/src/main/java/com/example/dell/listviewanimalsonidos/DetalleActivity.java
@@ -0,0 +1,39 @@
+package com.example.dell.listviewanimalsonidos;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetalleActivity extends AppCompatActivity {
+
+    ImageView foto;
+    TextView titulo;
+    TextView detalle;
+
+    MediaPlayer mp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle);
+
+        foto=(ImageView)findViewById(R.id.imgvFoto);
+        titulo=(TextView)findViewById(R.id.txtTitulo);
+        detalle=(TextView)findViewById(R.id.txtDetalle);
+
+        Datos obj = (Datos) getIntent().getExtras().getSerializable("objeto");
+        titulo.setText(obj.getTitulo());
+        detalle.setText(obj.getDetalle());
+        foto.setImageResource(obj.getImagen());
+
+        mp=MediaPlayer.create(this, obj.getSonido());
+
+    }
+
+    public void reproducir(View v){
+        mp.start();
+    }
+}
